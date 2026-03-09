bdd58c8dc1784801f7d8f8f2453f746fd33a23ec
unknown <ElAdmin@Genesis.(none)>
2026-03-09 14:07:30 -0400

    Cambios en el sistema del dispensarios

---
+++ DispensarioMedicoUnapec/Migrations/20260309175257_AgregarTablaVisitas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DispensarioMedicoUnapec.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarTablaVisitas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Visitas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Paciente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Medico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Motivo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Visitas");
+        }
+    }
+}
